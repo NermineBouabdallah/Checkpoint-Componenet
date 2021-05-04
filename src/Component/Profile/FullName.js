c74a6bd3e1aa2330784028fa0e612ddf55987623
DELL <nermine.bouabdallah@gmail.com>
2021-05-04 13:21:48 +0100

    first commit

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FullName() {
+    return (
+        <div>
+            <h1 style={{fontSize:40}}> Nermine Bouabdallah</h1>
+        </div>
+    )
+}
+
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto() {
+    return (
+        <div>
+            <img src="/imageInPub.jpg" alt="ProfilePhoto" style={{width:250,height:250}}/>
+        </div>
+    )
+}
+
+export default ProfilePhoto
